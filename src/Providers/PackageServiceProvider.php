343b737d838ddc79db2f3915c691bef5a416636a
ahmedshuaib <ahmedshuaib46@gmail.com>
2023-03-21 17:06:10 +0600

    fixing the bad method exception

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -10,7 +10,7 @@ class PackageServiceProvider extends ServiceProvider {
 
     public function boot() {
         if($this->app->runningInConsole()) {
-            $this->app->command([
+            $this->commands([
                 PackageGenerator::class,
             ]);
         }
